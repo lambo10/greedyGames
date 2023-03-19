db9ec768a93647befcae43e6b8a0751a41c2db24
DavyKing <yungdynamic53@gmail.com>
2023-03-19 08:40:43 +0100

    fixed data

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -287,8 +287,6 @@ class _SettingsState extends State<Settings> {
                                 ),
                               ),
                             );
-
-                            await reInstianteSeedRoot();
                           },
                           child: Container(
                             color: Colors.transparent,
