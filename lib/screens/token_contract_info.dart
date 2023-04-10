f330575ee9747e73e75cc63883517723d01b8e6e
DavyKing <yungdynamic53@gmail.com>
2023-04-10 09:11:10 +0100

    add view in block explorer for token viewer

--- lib/screens/token_contract_info.dart
+++ lib/screens/token_contract_info.dart
@@ -6,6 +6,9 @@ import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter_gen/gen_l10n/app_localization.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+import '../utils/app_config.dart';
 
 class TokenContractInfo extends StatefulWidget {
   final Map tokenData;
@@ -38,127 +41,164 @@ class _TokenContractInfoState extends State<TokenContractInfo> {
                   Padding(
                     padding: const EdgeInsets.all(25),
                     child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            AppLocalizations.of(context).token,
-                            style: const TextStyle(
-                              fontSize: 20,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          AppLocalizations.of(context).token,
+                          style: const TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            SizedBox(
+                              width: 40,
+                              height: 25,
+                              child: BlockieWidget(
+                                size: .6,
+                                data: widget.tokenData['contractAddress'],
+                              ),
                             ),
-                          ),
-                          const SizedBox(height: 20),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              SizedBox(
-                                width: 40,
-                                height: 25,
-                                child: BlockieWidget(
-                                  size: .6,
-                                  data: widget.tokenData['contractAddress'],
-                                ),
-                              ),
-                              const SizedBox(
-                                width: 10,
-                              ),
-                              Text(
-                                widget.tokenData['symbol'],
-                                style: const TextStyle(
-                                    fontSize: 16, color: Colors.grey),
-                              ),
-                            ],
-                          ),
-                          const SizedBox(height: 20),
-                          Text(
-                            AppLocalizations.of(context).contractAddress,
-                            style: const TextStyle(
-                              fontSize: 20,
+                            const SizedBox(
+                              width: 10,
                             ),
+                            Text(
+                              widget.tokenData['symbol'],
+                              style: const TextStyle(
+                                  fontSize: 16, color: Colors.grey),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 20),
+                        Text(
+                          AppLocalizations.of(context).contractAddress,
+                          style: const TextStyle(
+                            fontSize: 20,
                           ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          Row(
-                            children: [
-                              Text(
-                                ellipsify(
-                                  str: widget.tokenData['contractAddress'],
-                                  maxLength: 24,
-                                ),
-                                style: const TextStyle(
-                                    fontSize: 16, color: Colors.grey),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              ellipsify(
+                                str: widget.tokenData['contractAddress'],
+                                maxLength: 24,
                               ),
-                              const SizedBox(
-                                width: 10,
-                              ),
-                              GestureDetector(
-                                onTap: () async {
-                                  await Clipboard.setData(ClipboardData(
-                                    text: widget.tokenData['contractAddress'],
-                                  ));
+                              style: const TextStyle(
+                                  fontSize: 16, color: Colors.grey),
+                            ),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            GestureDetector(
+                              onTap: () async {
+                                await Clipboard.setData(ClipboardData(
+                                  text: widget.tokenData['contractAddress'],
+                                ));
 
-                                  ScaffoldMessenger.of(context).showSnackBar(
-                                    SnackBar(
-                                      content: Text(AppLocalizations.of(context)
-                                          .copiedToClipboard),
-                                      duration: const Duration(seconds: 2),
-                                    ),
-                                  );
-                                },
-                                child: const Icon(
-                                  Icons.copy,
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                    content: Text(AppLocalizations.of(context)
+                                        .copiedToClipboard),
+                                    duration: const Duration(seconds: 2),
+                                  ),
+                                );
+                              },
+                              child: const Icon(
+                                Icons.copy,
+                              ),
+                            )
+                          ],
+                        ),
+                        const SizedBox(height: 20),
+                        Text(
+                          AppLocalizations.of(context).symbol,
+                          style: const TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          widget.tokenData['symbol'],
+                          style:
+                              const TextStyle(fontSize: 16, color: Colors.grey),
+                        ),
+                        const SizedBox(height: 20),
+                        Text(
+                          AppLocalizations.of(context).decimals,
+                          style: const TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          '${widget.tokenData['decimals']}',
+                          style:
+                              const TextStyle(fontSize: 16, color: Colors.grey),
+                        ),
+                        const SizedBox(height: 20),
+                        Text(
+                          AppLocalizations.of(context).network,
+                          style: const TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          '${widget.tokenData['network']}',
+                          style:
+                              const TextStyle(fontSize: 16, color: Colors.grey),
+                        ),
+                        const SizedBox(height: 20),
+                        Container(
+                          color: Colors.transparent,
+                          width: double.infinity,
+                          height: 50,
+                          child: ElevatedButton(
+                            style: ButtonStyle(
+                              backgroundColor:
+                                  MaterialStateProperty.resolveWith(
+                                      (states) => appBackgroundblue),
+                              shape: MaterialStateProperty.resolveWith(
+                                (states) => RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
                                 ),
-                              )
-                            ],
-                          ),
-                          const SizedBox(height: 20),
-                          Text(
-                            AppLocalizations.of(context).symbol,
-                            style: const TextStyle(
-                              fontSize: 20,
+                              ),
+                              textStyle: MaterialStateProperty.resolveWith(
+                                (states) =>
+                                    const TextStyle(color: Colors.white),
+                              ),
                             ),
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          Text(
-                            widget.tokenData['symbol'],
-                            style: const TextStyle(
-                                fontSize: 16, color: Colors.grey),
-                          ),
-                          const SizedBox(height: 20),
-                          Text(
-                            AppLocalizations.of(context).decimals,
-                            style: const TextStyle(
-                              fontSize: 20,
+                            child: Text(
+                              AppLocalizations.of(context).viewOnBlockExplorer,
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black,
+                              ),
                             ),
+                            onPressed: () async {
+                              String blockExplorer =
+                                  widget.tokenData['blockExplorer'];
+                              blockExplorer = blockExplorer.replaceFirst(
+                                  '/tx/$transactionhashTemplateKey',
+                                  '/token/${widget.tokenData['contractAddress']}');
+                              await launchUrl(Uri.parse(blockExplorer));
+                            },
                           ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          Text(
-                            '${widget.tokenData['decimals']}',
-                            style: const TextStyle(
-                                fontSize: 16, color: Colors.grey),
-                          ),
-                          const SizedBox(height: 20),
-                          Text(
-                            AppLocalizations.of(context).network,
-                            style: const TextStyle(
-                              fontSize: 20,
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          Text(
-                            '${widget.tokenData['network']}',
-                            style: const TextStyle(
-                                fontSize: 16, color: Colors.grey),
-                          ),
-                          const SizedBox(height: 20),
-                        ]),
+                        ),
+                      ],
+                    ),
                   )
                 ],
               ),
